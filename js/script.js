49fad25bc5c57899386d9631ad30d42c23b7fd3f
Roman Pecherskikh <pwpec95@gmail.com>
2024-08-19 19:20:04 +0300

    19.08.2024d

--- js/script.js
+++ js/script.js
@@ -9,3 +9,30 @@ for (let i = 0; i < links.length; i++) {
         document.getElementById(links[i].getAttribute("data-")).scrollIntoView({behavior: "smooth"});
     }
 }
+
+const buttons = document.querySelectorAll(".products__item .button");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function() {
+        document.getElementById("order").scrollIntoView({behavior: "smooth"});
+    }
+}
+
+const prices = document.getElementsByClassName("products__bottom-left-price");
+
+document.getElementById("change-currency").onclick = function(e) {
+    const currentCurrency = e.target.innerText;
+
+    let newCurrency = "$";
+    let coefficient = 1;
+
+    if (currentCurrency === "$" ) {
+        newCurrency = "₽";
+        coefficient = 90;
+    } else if ( currentCurrency === "₽") {
+        newCurrency = "BYN";
+        coefficient = "3";
+    }
+    e.target.innerText = newCurrency;
+
+}
