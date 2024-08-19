7420a09ea4615e830e1129380ff8c782617830e6
Roman Pecherskikh <pwpec95@gmail.com>
2024-08-19 20:09:22 +0300

    19.08.2024e

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const links = document.querySelectorAll(".header__item > a");
 
 for (let i = 0; i < links.length; i++) {
     links[i].onclick = function() {
-        document.getElementById(links[i].getAttribute("data-")).scrollIntoView({behavior: "smooth"});
+        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior: "smooth"});
     }
 }
 
@@ -32,7 +32,38 @@ document.getElementById("change-currency").onclick = function(e) {
     } else if ( currentCurrency === "₽") {
         newCurrency = "BYN";
         coefficient = "3";
+    } else if (currentCurrency === 'BYN') {
+        newCurrency = '€';
+        coefficient = 0.9;
+    } else if (currentCurrency === '€') {
+        newCurrency = '¥';
+        coefficient = 6.9;
     }
     e.target.innerText = newCurrency;
 
+    for (let i = 0; i < prices.length; i++) {
+        prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
+    }
+}
+
+const product = document.getElementById("product");
+const name = document.getElementById("name");
+const phone = document.getElementById("phone");
+document.getElementById("order-action").onclick = function () {
+    let hasError = false;
+    [product, name , phone].forEach(item=> {
+        if (!item.value) {
+            item.style.borderColor = "red";
+            hasError = true;
+        } else {
+            item.style.borderColor = "";
+        }
+    });
+
+    if (!hasError) {
+        [product, name , phone].forEach(item=> {
+            item.value = "";
+        });
+        alert("Спасибо за заказ! Мы скоро свяжемся с вами")
+    }
 }
